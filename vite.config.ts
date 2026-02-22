e908332bf7cd5797cf5c39c4e59b479a37fcc2e4
sickkkalex <alessiosaulli4@gmail.com>
2026-02-22 23:13:50 +0100

    AAAAAA

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
     // Essendo la repo principale (nomeutente.github.io), la base è "/"
-    base: "/alessiosaulli.github.io/",
+    base: "/",
     server: {
         host: "::",
         port: 8080,
